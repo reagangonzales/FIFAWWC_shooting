3c90e89452ca89d11847fee013025570c3bca9b1
Chaely Raynor <chaely.raynor@stvincent.edu>
2026-06-04 14:08:36 -0400

    loading data

--- Raynor/Practice.R
+++ Raynor/Practice.R
@@ -1,1 +1,8 @@
 #Practice script
+#Chaely Raynor
+
+#Loading in the FIFAWWC shooting data
+library(tidyverse)
+
+wwc_shots <- read_csv("https://raw.githubusercontent.com/36-SURE/2026/main/data/wwc_shots.csv")
+View(wwc_shots)
